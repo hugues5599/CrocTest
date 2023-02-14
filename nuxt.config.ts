4ebab81b7a4323aa1761bd258418982a94c77b09
Notuc <98197211+Notuc@users.noreply.github.com>
2023-02-14 01:35:43 -0500

    Nuxt.3

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+
+export default defineNuxtConfig({
+    head: {
+      link: [
+        {
+        //   rel: 'stylesheet',
+        //   href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css'
+        }
+      ]
+    },
+    modules: [
+      '@nuxtjs/tailwindcss'
+    ]
+    
+  })
